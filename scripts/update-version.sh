a74b148c1b3ebe5e59dd4dabfba4ad31b1220143
Mitchell Stanton <mitchell.stanton@corespark.io>
2024-09-05 12:56:34 -0400

    Add notes and better validation

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+## This script is used to update the version of the project
+## - If you provide a symantec versioning number as an argument (I.e v1.2.3)
+##   it will update the version file with the provided version
+## - If you don't provide a version, it will check if the version file has a version
+##   (without updating the version file) and tag the repository with the version in the file
 
 # Check if the version is set
 versionToSet=$1
@@ -18,12 +23,21 @@ if [ "$versionToSet" != "" ]; then
     fi
 
     echo "Setting version to $versionToSet"
-    exit 0
-    #echo $versionToSet > pkg/builder/version
+    echo $versionToSet > pkg/builder/version
 fi
 
 # Get the version
 tag=`cat pkg/builder/version`
+
+# Ensure the version file has a version and that the version is valid
+if [ "$tag" == "" ]; then
+    echo "Version file does not have a version"
+    exit 1
+fi
+if [[ ! $tag =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Version in version file is not valid"
+    exit 1
+fi
 
 # Get the latest tag from git
 latest_tag=`git describe --tags --abbrev=0`
