d60d1199a1161fe6c048e43043bcfb1794a60dd3
Mitchell Stanton <mitchell.stanton@corespark.io>
2024-09-05 12:54:21 -0400

    Create script to make git tags and version easier to sync

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Check if the version is set
+versionToSet=$1
+if [ "$versionToSet" != "" ]; then
+    # Ensure the version follows the semantic versioning
+    if [[ ! $versionToSet =~ ^v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+
+        # It's acceptable to have the version without the v prefix
+        # But it will be appended to follow the same pattern
+        if  [[ $versionToSet =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        versionToSet="v$versionToSet"
+        else
+            echo "Version should follow the semantic versioning pattern (I.e v1.2.3)"
+            exit 1
+        fi
+
+    fi
+
+    echo "Setting version to $versionToSet"
+    exit 0
+    #echo $versionToSet > pkg/builder/version
+fi
+
 # Get the version
 tag=`cat pkg/builder/version`
 
